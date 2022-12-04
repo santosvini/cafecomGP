8c9b0ed77c3cb2d0ea626c422d7b210c3076e6f6
santosvini7 <vini_santos7@live.com>
2022-12-03 23:30:44 -0300

    :tada: Site café com Gerente de Projetos

---
+++ script.js
@@ -0,0 +1,37 @@
+function scrollSuave() {
+  const links = document.querySelectorAll('.js-menu a[href^="#"]')
+
+  function  scrollSections(event) {
+    event.preventDefault()
+
+    const href = event.currentTarget.getAttribute('href')
+    const section = document.querySelector(href)
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  }
+  links.forEach(link => {
+    link.addEventListener('click', scrollSections)
+  })
+}
+scrollSuave()
+
+function accordion() {
+  const accordionList = document.querySelectorAll('.accordion dt')
+  const activeClass = 'ativo'
+
+  if(accordionList.length) {
+    accordionList[0].classList.add(activeClass)
+    accordionList[0].nextElementSibling.classList.add(activeClass)
+
+    function activeAccord() {
+      this.classList.toggle(activeClass)
+      this.nextElementSibling.classList.toggle(activeClass)
+    }
+    accordionList.forEach(item => {
+      item.addEventListener('click', activeAccord)
+    })
+  }
+}
+accordion()
