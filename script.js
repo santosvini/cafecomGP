980ccf99b59d9fb28d9ae2ccdf5f3547fce23146
santosvini7 <vini_santos7@live.com>
2022-12-05 23:54:25 -0300

    add responsividade, aria, menu mobile

--- script.js
+++ script.js
@@ -1,46 +1,61 @@
 function scrollSuave() {
-  const links = document.querySelectorAll('.js-menu a[href^="#"]')
+  const links = document.querySelectorAll('.js-menu a[href^="#"]');
 
-  function  scrollSections(event) {
-    event.preventDefault()
+  function scrollSections(event) {
+    event.preventDefault();
 
-    const href = event.currentTarget.getAttribute('href')
-    const section = document.querySelector(href)
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
     section.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    })
+      behavior: "smooth",
+      block: "start",
+    });
   }
-  links.forEach(link => {
-    link.addEventListener('click', scrollSections)
-  })
+  links.forEach((link) => {
+    link.addEventListener("click", scrollSections);
+  });
 }
-scrollSuave()
 
 function accordion() {
-  const accordionList = document.querySelectorAll('.accordion dt')
-  const activeClass = 'ativo'
+  const accordionList = document.querySelectorAll(".accordion dt");
+  const activeClass = "ativo";
 
-  if(accordionList.length) {
-    accordionList[0].classList.add(activeClass)
-    accordionList[0].nextElementSibling.classList.add(activeClass)
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
 
     function activeAccord() {
-      this.classList.toggle(activeClass)
-      this.nextElementSibling.classList.toggle(activeClass)
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
     }
-    accordionList.forEach(item => {
-      item.addEventListener('click', activeAccord)
-    })
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccord);
+    });
   }
 }
-accordion()
 
-const btnMobile = document.getElementById('btn-mobile')
+function menuShow() {
+  const btnMobile = document.getElementById("btn-mobile");
 
-function toggleMenu() {
-  const menu = document.getElementById('nav')
-  menu.classList.toggle('active')
+  function toggleMenu(event) {
+    if (event.type === "touchstart") event.preventDefault();
+
+    const menu = document.getElementById("nav");
+    menu.classList.toggle("active");
+
+    const active = menu.classList.contains("active");
+    event.currentTarget.setAttribute("aria-expanded", active);
+
+    if (active) {
+      event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+    } else {
+      event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+    }
+  }
+  btnMobile.addEventListener("click", toggleMenu);
+  btnMobile.addEventListener("touchstart", toggleMenu);
 }
 
-btnMobile.addEventListener('click', toggleMenu)
+scrollSuave();
+accordion();
+menuShow();
