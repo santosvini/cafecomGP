0aff6dd2ed75337b85fec96a2e528d2a3084d068
santosvini7 <vini_santos7@live.com>
2022-12-06 23:43:33 -0300

    :iphone::technologist: ajustes nos scripts, add detalhes de layout e correcao de textos

--- script.js
+++ script.js
@@ -17,16 +17,18 @@ function scrollSuave() {
 }
 
 function accordion() {
+  
   const accordionList = document.querySelectorAll(".accordion dt");
   const activeClass = "ativo";
 
   if (accordionList.length) {
-    accordionList[0].classList.add(activeClass);
-    accordionList[0].nextElementSibling.classList.add(activeClass);
+    accordionList[0].classList.contains(activeClass);
+    accordionList[0].nextElementSibling.classList.contains(activeClass);
 
     function activeAccord() {
       this.classList.toggle(activeClass);
       this.nextElementSibling.classList.toggle(activeClass);
+      
     }
     accordionList.forEach((item) => {
       item.addEventListener("click", activeAccord);
